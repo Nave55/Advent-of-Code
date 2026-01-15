2cfb6ce0e144c92b80572721b899eb7c24649198
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2026-01-14 22:50:29 -0700

    Update day8.nim

--- Advent-of-Code-2024/nim/day8.nim
+++ Advent-of-Code-2024/nim/day8.nim
@@ -1,17 +1,15 @@
 import sequtils, strformat, tables, sets, tools
 
 type 
-    TI = tuple[x, y: int]
-    HTSTI = Table[TI, seq[TI]]
-    HCSTI = Table[char, seq[TI]]
-    SC = seq[char]
-    SSC = seq[SC]
+  Ants = Table[char, seq[TI]]
+  Slopes = Table[TI, seq[TI]]
+  Mat = seq[seq[char]]
 
 const 
-    width = 50
-    height = 50
+  width = 50
+  height = 50
 
-proc readInput(): (HCSTI, SSC) =
+proc readInput(): (Ants, Mat) =
   let file = open("input/day8.txt", fmRead);
   defer: file.close()
 
@@ -24,14 +22,18 @@ proc readInput(): (HCSTI, SSC) =
         discard result[0].hasKeyOrPut(c_val, @[])
         result[0][c_val] &= (r_ind, c_ind)
          
-func antSlopes(ants: HCSTI): HTSTI =
+func antSlopes(ants: Ants): Slopes =
   for value in ants.values():
     for i in 0..<value.len() - 1:
       for j in i+1..<value.len():
         discard result.hasKeyOrPut(value[i], @[])
         result[value[i]] &= value[i] - value[j]
 
-func solution(mat: SSC, slopes: HTSTI): int =
+func insert_set(pos: var TI, mat: Mat, set: var HashSet[string], symb: char) =
+  if inBounds(pos, width, height) and fetchVal(mat, pos) != symb:
+    set.incl(pos.tupToStr())
+
+func solution(mat: Mat, slopes: Slopes): int =
   var ttl = initHashSet[string]()
   for key, value in slopes:
     for i in value:
@@ -39,32 +41,30 @@ func solution(mat: SSC, slopes: HTSTI): int =
         symb = fetchVal(mat, key)
         pos = key + i
         neg = key - (i * 2)
-      if inBounds(pos, width, height) and fetchVal(mat, pos) != symb:
-        ttl.incl(pos.tupToStr())
-      if inBounds(neg, width, height) and fetchVal(mat, neg) != symb:
-        ttl.incl(neg.tupToStr())
+      insert_set(pos, mat, ttl, symb)
+      insert_set(neg, mat, ttl, symb)
         
   return ttl.len()
+
+func walk(start: TI, step: TI, set: var HashSet[string]) =
+  var pos = start
+  while true:
+    pos = pos + step
+    if pos.inBounds(width, height):
+      set.incl(pos.tupToStr())
+    else:
+      break
         
-func solution2(slopes: HTSTI): int =
+func solution2(slopes: Slopes): int =
   var ttl = initHashSet[string]()
+  
   for key, value in slopes:
     ttl.incl(key.tupToStr())
     for i in value:
       var val = key
-      while true:
-        val = val + i
-        if inBounds(val, width, height):
-          ttl.incl(val.tupToStr())
-        else:
-          val = key
-          break
-      while true:
-        val = val - i
-        if inBounds(val, width, height):
-          ttl.incl(val.tupToStr())
-        else: 
-          break
+      walk(val, i, ttl)
+      walk(val, i.neg(), ttl)
+        
   return ttl.len()
     
 let 
