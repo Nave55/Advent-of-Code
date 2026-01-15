5804cbf8cbf6fcd87d9e6e7009726282602c7945
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2026-01-14 22:29:27 -0700

    Update vec2.v

--- Advent-of-Code-2024/v/tools/vec2.v
+++ Advent-of-Code-2024/v/tools/vec2.v
@@ -26,11 +26,15 @@ pub fn (a Vec2[T]) to_str[T]() string {
 	return "${a.x},${a.y}"
 }
 
+pub fn (a Vec2[T]) print[T]() {
+	 println("[${a.x}, ${a.y}]")
+}
+
+pub fn (a Vec2[T]) neg[T]() Vec2[T] {
+	 return Vec2[T]{-a.x, -a.y}
+}
+
 pub fn str_to_vec2[T](s string) Vec2[T] {
 	tmp := s.split(",").map(it.int())
 	return Vec2[T]{tmp[0], tmp[1]}
 }
-
-// pub fn (a Vec2[T]) mul_by_scalar[T](val T) Vec2[T] {
-// 	return Vec2[T]{a.x * val, a.y * val}
-// }
