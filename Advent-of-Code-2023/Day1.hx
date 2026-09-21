32c6c78910d8d63c7f092a4704b4ef070b8160de
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2026-09-20 21:16:26 -0600

    Fix formatting and re-import necessary modules

--- Advent-of-Code-2023/Day1.hx
+++ Advent-of-Code-2023/Day1.hx
@@ -13,17 +13,19 @@ class Day1_2023 {
     }
 
     static inline function parsefile() {
-        var con:AS = [for (i in sys.io.File.getContent('Advent Files_2023/Day1.txt').split('\n')) trim(i)];
+        var con: AS = [for (i in sys.io.File.getContent('Advent Files_2023/Day1.txt').split('\n')) trim(i)];
         return con;
     }
 
     static function solution1(con: AS): Int {
         var ttl = 0;
         for (i in con) {
-            var tmp:AS = [];
+            var tmp: AS = [];
+            
             for (j in 0...i.length) {
                 if (i.charAt(j).isDigits()) tmp.push(i.charAt(j));
             }
+            
             ttl += parseInt(tmp[0] + tmp[tmp.length - 1]);
         }
         return ttl;
@@ -33,15 +35,18 @@ class Day1_2023 {
         var names = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
         var ttl = 0;
         for (i in con) {
-            var mp:MIS = new Map(); 
-            var arr:AI = [];
+            var mp: MIS = new Map(); 
+            var arr: AI = [];
+            
             for (j in 0...i.length) {
                 if (i.charAt(j).isDigits() == true) mp[j] = i.charAt(j);
             }
+            
             for (ind => val in names) {
                 mp[i.indexOf(val)] = string(ind+1);
                 mp[i.lastIndexOf(val)] = string(ind+1); 
             }
+            
             for (i in mp.keys()) if (i >= 0) arr.push(i);
             arr.sort((a,b) -> a-b);
             ttl += parseInt(mp[arr[0]] + mp[arr[arr.length - 1]]);
